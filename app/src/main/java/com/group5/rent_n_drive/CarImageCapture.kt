1cc6e014108aeafd24b3851eebd2874c311ee482
Rahul Edirisinghe <rahulediri@gmail.com>
2024-04-14 16:19:00 -0400

    Added Camera Functionality and Integrated with App, on Behalf of Saurav

--- app/src/main/java/com/group5/rent_n_drive/CarImageCapture.kt
+++ app/src/main/java/com/group5/rent_n_drive/CarImageCapture.kt
@@ -4,11 +4,8 @@ import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
 import android.net.Uri
-import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
@@ -16,8 +13,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
