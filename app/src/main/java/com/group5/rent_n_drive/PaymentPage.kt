4e188ad388c73ac557daf90fef3c97e402bc11d3
Ahmad abbas <ahmadabbas0091@gmail.com>
2024-04-14 20:27:03 -0400

    added payment validation and error messages

--- app/src/main/java/com/group5/rent_n_drive/PaymentPage.kt
+++ app/src/main/java/com/group5/rent_n_drive/PaymentPage.kt
@@ -8,6 +8,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -28,6 +31,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.group5.rent_n_drive.datastore.UserBookingDatastore
@@ -41,6 +45,7 @@ fun PaymentPage(navCon: NavController) {
     var cardHolderName by remember { mutableStateOf("") }
     var expiryDate by remember { mutableStateOf("") }
     var cvv by remember { mutableStateOf("") }
+    var showErrors by remember { mutableStateOf(false) }
     val appScope = rememberCoroutineScope()
     val userDatastoreRef = UserBookingDatastore(LocalContext.current)
     val userName = userDatastoreRef.getUserName.collectAsState(initial = "")
@@ -58,97 +63,106 @@ fun PaymentPage(navCon: NavController) {
     } catch (e: Exception) {
         false
     }
+    val isCardHolderNameValid = cardHolderName.all { it.isLetter() }
 
     // Determine if the "Pay Now" button should be enabled
-    val isPaymentEnabled = cardHolderName.isNotEmpty() && isCardNumberValid && isCvvValid && isExpiryDateValid
+    val isFormValid = isCardNumberValid && isCvvValid && isExpiryDateValid && isCardHolderNameValid
 
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text("Payment Details") },
-                navigationIcon = {
-                    IconButton(onClick = { navCon.popBackStack() }) {
-                        Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
-                    }
-                }
-            )
-        },
-        content = {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Spacer(modifier = Modifier.height(30.dp))
-                OutlinedTextField(
-                    modifier = Modifier
-                        .padding(start = 20.dp, end = 20.dp)
-                        .fillMaxWidth(),
-                    value = cardHolderName,
-                    onValueChange = { cardHolderName = it },
-                    label = {
-                        androidx.compose.material3.Text(
-                            text = "CARD HOLDER NAME",
-                            color = Color.Blue
-                        )
-                    },
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(30.dp))
+        OutlinedTextField(
+            modifier = Modifier
+                .padding(start = 20.dp, end = 20.dp)
+                .fillMaxWidth(),
+            value = cardHolderName,
+            onValueChange = { cardHolderName = it },
+            label = {
+                androidx.compose.material3.Text(
+                    text = "CARD HOLDER NAME",
+                    color = Color.Blue
                 )
-                Spacer(modifier = Modifier.height(16.dp))
-                OutlinedTextField(
-                    modifier = Modifier
-                        .padding(start = 20.dp, end = 20.dp)
-                        .fillMaxWidth(),
-                    value = cardNumber,
-                    onValueChange = { cardNumber = it },
-                    label = {
-                        androidx.compose.material3.Text(
-                            text = "CARD NUMBER",
-                            color = Color.Blue
-                        )
-                    },
-                )
-
-                Spacer(modifier = Modifier.height(16.dp))
-                OutlinedTextField(
-                    modifier = Modifier
-                        .padding(start = 20.dp, end = 20.dp)
-                        .fillMaxWidth(),
-                    value = expiryDate,
-                    onValueChange = { expiryDate = it },
-                    label = {
-                        androidx.compose.material3.Text(
-                            text = "EXPIRY DATE (MM/YY)",
-                            color = Color.Blue
-                        )
-                    },
-                )
-                Spacer(modifier = Modifier.height(16.dp))
-                OutlinedTextField(
-                    modifier = Modifier
-                        .padding(start = 20.dp, end = 20.dp)
-                        .fillMaxWidth(),
-                    value = cvv,
-                    onValueChange = { cvv = it },
-                    label = {
-                        androidx.compose.material3.Text(
-                            text = "CVV",
-                            color = Color.Blue
-                        )
-                    },
-                )
-                Spacer(modifier = Modifier.height(16.dp))
-                Button(onClick = {
-                    val newDestination = "confirmation"
-                    appScope.launch{
-                        userDatastoreRef.savePreviousUserInformation(userName.value!!)
-                    }
-                    navCon.navigate("loading/${newDestination}")
-                }, enabled = isPaymentEnabled) {
-                    Text("PAY NOW")
-                }
-            }
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+        )
+        if (showErrors && !isCardHolderNameValid) {
+            Text("Name should only contain letters", color = Color.Red)
         }
-    )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            modifier = Modifier
+                .padding(start = 20.dp, end = 20.dp)
+                .fillMaxWidth(),
+            value = cardNumber,
+            onValueChange = { cardNumber = it },
+            label = {
+                androidx.compose.material3.Text(
+                    text = "CARD NUMBER",
+                    color = Color.Blue
+                )
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        if (showErrors && !isCardNumberValid) {
+            Text("Card number should be 16 digits", color = Color.Red)
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            modifier = Modifier
+                .padding(start = 20.dp, end = 20.dp)
+                .fillMaxWidth(),
+            value = expiryDate,
+            onValueChange = { expiryDate = it },
+            label = {
+                androidx.compose.material3.Text(
+                    text = "EXPIRY DATE (MM/YY)",
+                    color = Color.Blue
+                )
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        if (showErrors && !isExpiryDateValid) {
+            Text("Expiry date should be in MM/YY format and valid", color = Color.Red)
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            modifier = Modifier
+                .padding(start = 20.dp, end = 20.dp)
+                .fillMaxWidth(),
+            value = cvv,
+            onValueChange = { cvv = it },
+            label = {
+                androidx.compose.material3.Text(
+                    text = "CVV",
+                    color = Color.Blue
+                )
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        if (showErrors && !isCvvValid) {
+            Text("CVV should be 3 or 4 digits", color = Color.Red)
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            if (isFormValid) {
+                val newDestination = "confirmation"
+                appScope.launch {
+                    userDatastoreRef.savePreviousUserInformation(userName.value!!)
+                }
+                navCon.navigate("loading/${newDestination}")
+            } else {
+                showErrors = true
+            }
+        }) {
+            Text("PAY NOW")
+        }
+    }
 }
+
+
