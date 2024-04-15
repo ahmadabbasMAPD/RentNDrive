ded19706dffa99ca835248d617931812df0e8284
Rahul Edirisinghe <rahulediri@gmail.com>
2024-04-14 22:38:42 -0400

    Added some comments

--- app/src/main/java/com/group5/rent_n_drive/MyImage.kt
+++ app/src/main/java/com/group5/rent_n_drive/MyImage.kt
@@ -5,11 +5,13 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
 /*
 * Made by Ahmad Abbas, Rahul Edirisinghe, Calist Dsouza, Saurav Gautam for MAPD721 Project.
 * Class notes, and Other references and online resources used to help write code given below.
 * */
+@OptIn(ExperimentalCoilApi::class)
 @Composable
 fun MyImage(imageUrl: String) {
     Image(
