e6e557667baf3712298b3bcbbf693fdef6744072
Rahul Edirisinghe <rahulediri@gmail.com>
2024-04-14 22:38:25 -0400

    Added some comments

--- app/src/main/java/com/group5/rent_n_drive/MyImage.kt
+++ app/src/main/java/com/group5/rent_n_drive/MyImage.kt
@@ -1,17 +1,11 @@
 package com.group5.rent_n_drive
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.text.font.FontWeight
 import coil.compose.rememberImagePainter
-import com.group5.rent_n_drive.Car
 /*
 * Made by Ahmad Abbas, Rahul Edirisinghe, Calist Dsouza, Saurav Gautam for MAPD721 Project.
 * Class notes, and Other references and online resources used to help write code given below.
